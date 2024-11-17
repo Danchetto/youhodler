2ccc7ff2df9fde95608d72fc8b9114261b5c6846
Daniil Tuchin <danchetto@yandex.ru>
2024-11-17 21:34:54 +0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,12 @@ import { checkCache } from './cache';
 
 const app: Express = express();
 
-console.log(config);
-
 const port = config.PORT;
 
 app.use(express.json());
 app.use(checkCache);
 app.use('/binance', binanceRouter);
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Hello!');
-});
-
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at port ${port}`);
 });
