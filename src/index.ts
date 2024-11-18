ce5538df75c952ff807d0688c19fb0b45a84dee2
Daniil Tuchin <danchetto@yandex.ru>
2024-11-18 10:17:00 +0400

    reformat

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express } from 'express';
 import binanceRouter from './routes/binance';
 import config from './config';
 import { checkCache } from './cache';
--- src/services/binanceService.ts
+++ src/services/binanceService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import addComission from '../utils/comission';
-import config from "../config";
+import config from '../config';
 
 export const fetchBitcoinData = async () => {
   const apiUrl = config.BINANCE_API_URL;
